f81941d9682ba0f8b588c2c1c5e4a517e7e28483
PeterShipp <pshipp81@aol.com>
2020-03-03 13:17:10 -0800

    Add project files.

---
+++ Lesson6_3_OOP/Animal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson6_3_OOP
+{
+    class Animal
+    {
+        private int legs { get; set; }
+        private int arms { get; set; }
+        private string type { get; set; }
+        private string name { get; set; }
+        private bool isDangerous { get; set; }
+
+
+        public Animal(int legs, int arms, string type, string name, bool isDangerous)
+        {
+            this.legs = legs;
+            this.arms = arms;
+            this.name = name;
+            this.isDangerous = isDangerous;
+
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Type of Animal: " + type);
+            Console.WriteLine("Number of arms: " + arms);
+            Console.WriteLine("Number of legs: " + legs);
+            Console.WriteLine("Is Dangerous? " + isDangerous);
+
+        }
+
+        public void SendToObedienceSchool()
+        {
+            if (isDangerous)
+            {
+                isDangerous = false;
+                Console.WriteLine(name + "is a good" + type + "now!");
+            }
+            else
+            {
+                Console.WriteLine("But" + name + "isn't dangerous!");
+            }
+
+
+        }
+
+        public void Evolve(int newArms, int newLegs)
+        {
+            arms = newArms;
+            legs = newLegs;
+            type = "Something Else!";
+        }
+    }
+    }
+
---
+++ Lesson6_3_OOP/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson6_3_OOP
+{
+    class Program
+    {
+        static void Main(String[] arg)
+        {
+            CoffeeMug mug1 = new CoffeeMug("white", "black", "World's Best Daddy", 16);
+            CoffeeMug mug2 = new CoffeeMug("black", "gold", "Rise & Shine", 12);
+
+            Console.WriteLine(mug1);
+            Console.WriteLine(mug2);
+
+            mug1.Fill();
+            mug1.Drink();
+            mug1.Drink();
+            mug1.Fill();
+
+            Animal animal1 = new Animal(4, 0, "Cat", "Snowball", true);
+            Animal animal2 = new Animal(8, 0, "Spider", "Friendly the Spider", false);
+            Animal animal3 = new Animal(2, 2, "Monkey", "Bobo", false);
+
+            animal1.ShowDetails();
+            animal2.ShowDetails();
+            animal3.ShowDetails();
+        }
+    }
+   
+    
+}
